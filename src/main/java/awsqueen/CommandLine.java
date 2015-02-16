f8f927563a3e520ba481e36fcf72ba1cbbb52936
Gregsen <gkneitschel@gmail.com>
2015-02-16 22:09:48 +0100

    Random files for a complete structure

---
+++ src/main/java/awsqueen/CommandLine.java
@@ -0,0 +1,10 @@
+package awsqueen;
+
+/**
+ * Created by greg on 16.02.15.
+ */
+public class CommandLine {
+    public static void main(String[] args) {
+        System.out.println("I can haz commandline");
+    }
+}
---
+++ src/test/java/awsqueen/CommandLineTest.java
@@ -0,0 +1,10 @@
+package awsqueen;
+
+/**
+ * Created by greg on 16.02.15.
+ */
+public class CommandLineTest {
+    public static void main(String[] args) {
+        System.out.println("Pass ;)");
+    }
+}
