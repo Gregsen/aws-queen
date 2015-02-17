34a354b492e2d142efa5bf707c791d3973f0efbf
Gregsen <gkneitschel@gmail.com>
2015-02-17 23:09:16 +0100

    Class for credentials

---
+++ src/main/java/awsqueen/aws/AwsConnector.java
@@ -0,0 +1,19 @@
+package awsqueen.aws;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+
+/**
+ * Created by greg on 17.02.15.
+ */
+public class AwsConnector {
+    private AWSCredentials awsCredentials;
+
+    public void setAwsCredentials(AWSCredentials awsCredentials) {
+        this.awsCredentials = new ProfileCredentialsProvider().getCredentials();
+    }
+
+    public AWSCredentials getAwsCredentials() {
+        return awsCredentials;
+    }
+}
--- src/main/java/awsqueen/commands/AwsCommands.java
+++ src/main/java/awsqueen/commands/AwsCommands.java
@@ -1,5 +1,6 @@
 package awsqueen.commands;
 
+import awsqueen.aws.AwsConnector;
 import org.springframework.shell.core.CommandMarker;
 import org.springframework.shell.core.annotation.CliCommand;
 import org.springframework.shell.core.annotation.CliOption;
@@ -12,6 +13,11 @@ public class AwsCommands implements CommandMarker {
     @CliCommand(value = "awq", help = "Print information for this context")
     public String list(@CliOption(key = {"test"}, mandatory = false, help = "as list") final String asList) {
         return "here should we run a ls for the context requested";
+    }
+
+    @CliCommand(value = "awcredentials", help = "Print AWS Crendentials")
+    public String credentials(){
+        return new AwsConnector().getAwsCredentials().toString();
     }
 
 }
