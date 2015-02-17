7429dacf93d0044deea0dfcd9dd8ebcb0dfc6b78
Michele Randi <michele.randi@zalando.de>
2015-02-17 11:21:08 +0100

    use spring shell

---
+++ src/main/java/awsqueen/Main.java
@@ -0,0 +1,13 @@
+package awsqueen;
+
+
+import org.springframework.shell.Bootstrap;
+
+/**
+ * @author mrandi
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Bootstrap.main(args);
+    }
+}
