d305fd87870ce40e8053c4edd1f4f238c5d3665c
Gregsen <gkneitschel@gmail.com>
2015-02-22 19:31:16 +0100

    added method for removing region folders

--- src/main/java/awsqueen/aws/Utils.java
+++ src/main/java/awsqueen/aws/Utils.java
@@ -1,10 +1,22 @@
+/*
+ * Copyright (C) 2015 aws-queen team
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses>.
+ */
+
 package awsqueen.aws;
 
 import awsqueen.aws.services.AwsServiceProvider;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.regions.Regions;
+import org.apache.commons.io.FileUtils;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -45,5 +57,24 @@ public class Utils {
             }
         }
 
+    }
+
+    public String rmDirs(String force) {
+        if (!force.isEmpty() && !force.equals("true")){
+            return "Unknown sequence: "+force;
+        }
+        for (Regions region : Regions.values()) {
+            File directory = new File(region.getName());
+            if(force.isEmpty()){
+                try { //TODO deletes non-emtpy directories...should not work, works anyway
+                    FileUtils.deleteDirectory(directory);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+              FileUtils.deleteQuietly(directory);
+            }
+        }
+        return "";
     }
 }
--- src/main/java/awsqueen/commands/AwsCommands.java
+++ src/main/java/awsqueen/commands/AwsCommands.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright (C) 2015 aws-queen team
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses>.
+ */
+
 package awsqueen.commands;
 
 import awsqueen.aws.AwsConnector;
@@ -7,11 +17,9 @@ import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.ec2.AmazonEC2Client;
 import com.amazonaws.services.ec2.model.DescribeInstancesResult;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.shell.core.CommandMarker;
 import org.springframework.shell.core.annotation.CliCommand;
 import org.springframework.shell.core.annotation.CliOption;
-
 import org.springframework.stereotype.Component;
 
 
@@ -43,19 +51,27 @@ public class AwsCommands implements CommandMarker {
         return "here should we run a ls for the context requested";
     }
 
-    @CliCommand(value = "awsinit",help = "Initialise a AWS based directory structure. Run this before everything else")
+    @CliCommand(value = "awsinit", help = "Initialise a AWS based directory structure. Run this before everything else")
     public void awsInit(){
         new Utils().initDirs();
     }
 
-    @CliCommand(value = "awcredentials", help = "Print AWS Crendentials")
+    @CliCommand(value = "awsrm", help = "removes folders created by awsinit. By default, only empty folders will be deleted")
+    public String awsRm(
+            @CliOption(key = {"force"}, mandatory = false, help = "force deletion of non-empty folders", specifiedDefaultValue= "true", unspecifiedDefaultValue = "")
+                final String force){
+        return new Utils().rmDirs(force);
+
+    }
+
+    @CliCommand(value = "awscredentials", help = "Print AWS Credentials")
     public String credentials(){
         AWSCredentials awsCredentials = awsConnector.getAwsCredentials();
         String output = awsCredentials.getAWSAccessKeyId() + " - " + awsCredentials.getAWSSecretKey();
         return output;
     }
 
-    @CliCommand(value = "lsec2", help = "")
+    @CliCommand(value = "awsls", help = "List services within a region")
     public String lsEc2(){
         AmazonEC2Client amazonEC2Client = new AmazonEC2Client(awsConnector.getAwsCredentials());
         amazonEC2Client.setRegion(Region.getRegion(Regions.EU_CENTRAL_1));
