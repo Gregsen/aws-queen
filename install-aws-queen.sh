9a988826bf9088f1078a14bc6255ee4ffdc0ac2b
Gregsen <gkneitschel@gmail.com>
2015-03-15 21:21:25 +0100

    compiling as user instead of root.
    Removing sudo for the whole file, so you compile as user. Otherwise, you jars will be owned by root.

--- install-aws-queen.sh
+++ install-aws-queen.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 set -e
 
-if [ "$(id -u)" != "0" ]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
-fi
-
+sudo -s <<EOF
 if [ ! -d /opt/awsqueen/lib ]; then
 	mkdir -p /opt/awsqueen/lib
 fi
@@ -13,7 +9,7 @@ fi
 if [ ! -d /opt/awsqueen/bin ]; then
 	mkdir -p /opt/awsqueen/bin
 fi
-
+EOF
 
 if [[ ! $(type "mvn") > /dev/null ]]; then
   echo "your need maven to run this script\n"
@@ -24,7 +20,7 @@ fi
 
 mvn clean package
 
-
+sudo -s <<EOF
 cp awqregion/target/awqregion-0.1-SNAPSHOT.jar /opt/awsqueen/lib
 cp awqfetch/target/awqfetch-0.1-SNAPSHOT.jar /opt/awsqueen/lib
 
@@ -35,6 +31,7 @@ for file in scripts/*.sh; do
 done
 
 chmod +x /opt/awsqueen/bin/*
+EOF
 
 echo "awsqueen successfully installed!"
 
